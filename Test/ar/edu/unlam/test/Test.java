bc5c1a1809b87488f16f6800036f6e6b545a243f
alexisvh5 <109443139+alexisvh5@users.noreply.github.com>
2024-05-27 15:50:18 -0300

    testAlexis
    ver el test calcularComisionDelEmpleado...incompleto, falta test de agregarEmpleado

--- Test/ar/edu/unlam/test/Test.java
+++ Test/ar/edu/unlam/test/Test.java
@@ -2,6 +2,8 @@ package ar.edu.unlam.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 
 import ar.edu.unalm.enums.Categoria;
@@ -32,7 +34,7 @@ public class Test {
 		this.tiendaDeCalzado = new TiendaDeCalzado(NOMBRE_TIENDA_CALZADO);
 	}
 
-	//Faa
+	//
 	@org.junit.Test
 	public void queSePuedaAnadirCalzadoBotinALaTienda() {
 
@@ -143,18 +145,183 @@ public class Test {
 	@org.junit.Test
 	public void queTraigaTodosLosRunning() {
 
+		Integer idCalzado = 1;
+		Integer talle = 36;
+		Integer stock = 10;
+		String color = "Negro";
+		Genero genero = Genero.MASCULINO;
+		Double precio = 20.0;
+		Marca marca = Marca.TOPPER;
+		TipoDePisada tipoPisada = TipoDePisada.PISADA_NEUTRA;
+		Calzado running = new Running(idCalzado, talle, stock, color, genero, precio, marca, tipoPisada);
+		Calzado running2 = new Running(2, 40, 12, "Azul", Genero.FEMENINO, 35.0, Marca.ADIDAS,
+				TipoDePisada.PISADA_SUPINADORA);
+
+		// Agregar calzadoBuscado a local
+
+		this.tiendaDeCalzado.agregarCalzado(running);
+		this.tiendaDeCalzado.agregarCalzado(running2);
+
+		// ahora agrego otro tipo
+
+		Integer idCalzadoOut = 1;
+		Integer talleOut = 36;
+		Integer stockOut = 10;
+		String colorOut = "Negro";
+		Genero generoOut = Genero.MASCULINO;
+		Double precioOut = 20.0;
+
+		Marca marcaOut = Marca.TOPPER;
+
+		TipoDeUso tipoDeUsoOut = TipoDeUso.HIKING;
+
+		Calzado outDoor = new OutDoor(idCalzadoOut, talleOut, stockOut, colorOut, generoOut, precioOut, marcaOut,
+				tipoDeUsoOut);
+		Calzado botin = new Botin(3, 33, 3, "Blanco", Genero.MASCULINO, 333.0, Marca.JOHN_FOOS,
+				TipoSuperficie.SUELO_BLANDO);
+		this.tiendaDeCalzado.agregarCalzado(outDoor);
+		this.tiendaDeCalzado.agregarCalzado(botin);
+		
+		//verifico
+
+		List<Running>calzadosRunning = this.tiendaDeCalzado.obtenerTodosLosRunning();
+		
+		assertEquals(2,calzadosRunning.size());
+
 	}
 
 	@org.junit.Test
 	public void queTraigaTodosLosOutDoor() {
+		
+		Integer idCalzado = 1;
+		Integer talle = 36;
+		Integer stock = 10;
+		String color = "Negro";
+		Genero genero = Genero.MASCULINO;
+		Double precio = 20.0;
+		Marca marca = Marca.TOPPER;
+		TipoDePisada tipoPisada = TipoDePisada.PISADA_NEUTRA;
+		Calzado running = new Running(idCalzado, talle, stock, color, genero, precio, marca, tipoPisada);
+		Calzado running2 = new Running(2, 40, 12, "Azul", Genero.FEMENINO, 35.0, Marca.ADIDAS,
+				TipoDePisada.PISADA_SUPINADORA);
+
+		this.tiendaDeCalzado.agregarCalzado(running);
+		this.tiendaDeCalzado.agregarCalzado(running2);
+		
+		Calzado botin = new Botin(3, 33, 3, "Blanco", Genero.MASCULINO, 333.0, Marca.JOHN_FOOS,
+				TipoSuperficie.SUELO_BLANDO);
+		
+		this.tiendaDeCalzado.agregarCalzado(botin);
+		
+
+		// ahora agrego de Tipo que busco
+
+		Integer idCalzadoOut = 1;
+		Integer talleOut = 36;
+		Integer stockOut = 10;
+		String colorOut = "Negro";
+		Genero generoOut = Genero.MASCULINO;
+		Double precioOut = 20.0;
+
+		Marca marcaOut = Marca.TOPPER;
+
+		TipoDeUso tipoDeUsoOut = TipoDeUso.HIKING;
+
+		Calzado outDoor = new OutDoor(idCalzadoOut, talleOut, stockOut, colorOut, generoOut, precioOut, marcaOut,
+				tipoDeUsoOut);
+		Calzado outDoor2 = new OutDoor(2, 43, 11, "Gris", Genero.MASCULINO, 100.0, Marca.NIKE,
+				TipoDeUso.HIKING);
+		Calzado outDoor3 = new OutDoor(3, 47, 3, "Negro", Genero.FEMENINO, 200.0, Marca.NIKE,
+				tipoDeUsoOut);
+		
+		this.tiendaDeCalzado.agregarCalzado(outDoor);
+		this.tiendaDeCalzado.agregarCalzado(outDoor2);
+		this.tiendaDeCalzado.agregarCalzado(outDoor3);
+	
+		
+		//verifico
+
+		List<OutDoor>calzadosOutDoor = this.tiendaDeCalzado.obtenerTodosLosOutDoor();
+		
+		assertEquals(3,calzadosOutDoor.size());
 	}
 
 	@org.junit.Test
-	public void queTraigaTodosLosBotin() {
+	public void queTraigaTodosLosBotin() { //repito codigo desordenado
+		
+		Integer idCalzado = 1;
+		Integer talle = 36;
+		Integer stock = 10;
+		String color = "Negro";
+		Genero genero = Genero.MASCULINO;
+		Double precio = 20.0;
+		Marca marca = Marca.TOPPER;
+		TipoDePisada tipoPisada = TipoDePisada.PISADA_NEUTRA;
+		Calzado running = new Running(idCalzado, talle, stock, color, genero, precio, marca, tipoPisada);
+		Calzado running2 = new Running(2, 40, 12, "Azul", Genero.FEMENINO, 35.0, Marca.ADIDAS,
+				TipoDePisada.PISADA_SUPINADORA);
+
+		this.tiendaDeCalzado.agregarCalzado(running);
+		this.tiendaDeCalzado.agregarCalzado(running2);
+		
+		Calzado botin = new Botin(3, 33, 3, "Blanco", Genero.MASCULINO, 333.0, Marca.JOHN_FOOS,
+				TipoSuperficie.SUELO_BLANDO);
+		
+		this.tiendaDeCalzado.agregarCalzado(botin);
+		
+
+		Integer idCalzadoOut = 1;
+		Integer talleOut = 36;
+		Integer stockOut = 10;
+		String colorOut = "Negro";
+		Genero generoOut = Genero.MASCULINO;
+		Double precioOut = 20.0;
+
+		Marca marcaOut = Marca.TOPPER;
+
+		TipoDeUso tipoDeUsoOut = TipoDeUso.HIKING;
+
+		Calzado outDoor = new OutDoor(idCalzadoOut, talleOut, stockOut, colorOut, generoOut, precioOut, marcaOut,
+				tipoDeUsoOut);
+		Calzado outDoor2 = new OutDoor(2, 43, 11, "Gris", Genero.MASCULINO, 100.0, Marca.NIKE,
+				TipoDeUso.HIKING);
+		Calzado outDoor3 = new OutDoor(3, 47, 3, "Negro", Genero.FEMENINO, 200.0, Marca.NIKE,
+				tipoDeUsoOut);
+		
+		this.tiendaDeCalzado.agregarCalzado(outDoor);
+		this.tiendaDeCalzado.agregarCalzado(outDoor2);
+		this.tiendaDeCalzado.agregarCalzado(outDoor3);
+	
+		
+		//verifico
+
+		List<Botin>calzadosBotin = this.tiendaDeCalzado.obtenerTodosLosBotin();
+		
+		assertEquals(1,calzadosBotin.size());
 	}
 
 	@org.junit.Test
 	public void calcularComisionDelEmpleado() {
+		
+
+		String nombre = "Empleado";
+		TipoContrato modalidadDeContratacion = TipoContrato.TIEMPO_INDETERMINDADO;
+		Integer legajo = 1111;
+		TipoDeEmpleado tipoDeEmpleado = TipoDeEmpleado.REPOSITOR;
+		Integer antiguedad = 20;
+		Categoria categoria = Categoria.FULL_TIME;
+
+		Empleado empleado = new Empleado(nombre, modalidadDeContratacion, legajo, tipoDeEmpleado, antiguedad,
+				categoria);
+		
+		// deberia agregar un empleado al array  tienda de calzado y el test de que se pueda crear un empleado teberia tener el agregado ahi?
+
+		Integer comision = this.tiendaDeCalzado.calcularComisionEmpleado(empleado);
+		
+		
+		// seria 20 * 6 * 9
+		assertEquals(1080, (int)comision);
+		
 	}
 
 	// Kevin
--- src/ar/edu/unlam/dominio/TiendaDeCalzado.java
+++ src/ar/edu/unlam/dominio/TiendaDeCalzado.java
@@ -3,18 +3,89 @@ package ar.edu.unlam.dominio;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unalm.enums.TipoContrato;
+
 public class TiendaDeCalzado {
 	private String nombreTienda;
 
 	private List<Calzado> calzados;
+	private List<Empleado> empleados;
 
 	public TiendaDeCalzado(String nombreTienda) {
 		this.nombreTienda = nombreTienda;
 		this.calzados = new ArrayList<>();
+		this.empleados = new ArrayList<>();
 	}
 
 	public Boolean agregarCalzado(Calzado calzado) {
 		return this.calzados.add(calzado);
 	}
 
+	public List<Running> obtenerTodosLosRunning() {
+
+		List<Running> calzadosRunning = new ArrayList<>();
+
+		for (Calzado calzado : calzados) {
+			if (calzado instanceof Running) {
+				calzadosRunning.add((Running) calzado);
+			}
+		}
+		return calzadosRunning;
+
+	}
+
+	public List<OutDoor> obtenerTodosLosOutDoor() {
+		List<OutDoor> calzadosOutDoor = new ArrayList<>();
+
+		for (Calzado calzado : calzados) {
+			if (calzado instanceof OutDoor) {
+				calzadosOutDoor.add((OutDoor) calzado);
+			}
+		}
+		return calzadosOutDoor;
+	}
+
+	public List<Botin> obtenerTodosLosBotin() {
+
+		List<Botin> calzadosBotin = new ArrayList<>();
+
+		for (Calzado calzado : calzados) {
+			if (calzado instanceof Botin) {
+				calzadosBotin.add((Botin) calzado);
+			}
+		}
+		return calzadosBotin;
+
+	}
+
+	public Integer calcularComisionEmpleado(Empleado empleadoBuscado) {
+		Integer comision = 0;
+
+		for (Empleado empleado : this.empleados) {
+		    if (empleado.getLegajo().equals(empleadoBuscado.getLegajo())) {
+		        switch (empleado.getTipoContrato()) {
+		            case PRUEBA:
+		                comision = empleado.getAntiguedad() * 2;
+		                break;
+		            case PASANTIA:
+		                comision = empleado.getAntiguedad() * 4;
+		                break;
+		            case TIEMPO_INDETERMINDADO:
+		                comision = empleado.getAntiguedad() * 6;
+		                break;
+		        }
+		        
+		        switch (empleado.getCategoria()) {
+		            case FULL_TIME:
+		                comision *= 7;
+		                break;
+		            case PART_TIME:
+		                comision *= 9;
+		                break;
+		        }
+		    }
+		}
+
+		return comision;
+	}
 }
